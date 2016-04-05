411722aff38cab78340907c660058c3ff0f0739d
nilva <nilvinha.rodrigues@hotmail.com>
2016-04-05 15:22:47 -0300

    Novo repositorio

---
+++ HelloWorld/HelloWorld/src/br/edu/ifg/Outro.java
@@ -0,0 +1,12 @@
+package br.edu.ifg;
+
+public class Outro {
+	
+	String Hello;
+
+	public  String imprima(String Hello){
+		this.Hello = Hello;
+		
+		return Hello;
+	}
+}
---
+++ HelloWorld/HelloWorld/src/br/edu/ifg/Um.java
@@ -0,0 +1,13 @@
+package br.edu.ifg;
+
+public class Um {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Outro outro = new Outro();
+		
+		outro.imprima("OI MUNDO");
+	}
+
+}
